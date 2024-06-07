8ed0f0faba994303b8424675a5bc8d56ed1d1c40
Nicolas Meyer <myr.nicolas@gmail.com>
2024-06-07 18:26:40 +0200

    first commit - bundle gem temporal_enum

---
+++ lib/temporal_enum.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "temporal_enum/version"
+
+module TemporalEnum
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/temporal_enum_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TemporalEnumTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::TemporalEnum::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
