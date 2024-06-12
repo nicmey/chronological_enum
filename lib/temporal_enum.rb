f1cbd8dae97742152ecdd7c8a55ce3ed5bced548
Nicolas Meyer <myr.nicolas@gmail.com>
2024-06-12 19:52:37 +0200

    simplify code and handle latest way of defining enums

--- lib/temporal_enum.rb
+++ lib/temporal_enum.rb
@@ -14,85 +14,35 @@ module TemporalEnum
   class Error < StandardError; end
 
   def enum(name = nil, values = nil, **options)
-    temporal = options[:_temporal]
-    options.delete(:_temporal)
+    temporal = options.delete(:_temporal)
     super(name, values, **options)
 
     return unless temporal
 
-    add_enum_temporal_methods(options.keys.first)
-    check_enum_values!(options.keys.first)
+    enum_name = name || options.keys.first
+    check_enum_values!(enum_name)
+    add_enum_temporal_scopes(enum_name: enum_name, prefix: options[:_prefix], suffix: options[:_suffix])
   end
 
   private
 
-  def add_enum_temporal_methods(enum_name)
-    prefix, suffix = prefix_or_suffix(enum_name)
+  def add_enum_temporal_scopes(enum_name:, prefix: nil, suffix: nil)
+    prefix = if prefix
+               prefix == true ? "#{enum_name}_" : "#{prefix}_"
+             end
+
+    suffix = if suffix
+               suffix == true ? "_#{enum_name}" : "_#{suffix}"
+             end
 
     send(enum_name.to_s.pluralize).each do |key, value|
-      method_name = if prefix.present? && suffix.present?
-                      "#{prefix}_#{key}_#{suffix}"
-                    elsif prefix.present?
-                      "#{prefix}_#{key}"
-                    elsif suffix.present?
-                      "#{key}_#{suffix}"
-                    else
-                      key
-                    end
+      method_name = "#{prefix}#{key}#{suffix}"
 
       scope "after_#{method_name}", -> { where("#{enum_name} > ?", value) }
       scope "before_#{method_name}", -> { where("#{enum_name} < ?", value) }
       scope "after_or_#{method_name}", -> { where("#{enum_name} >= ?", value) }
       scope "before_or_#{method_name}", -> { where("#{enum_name} <= ?", value) }
     end
-  end
-
-  # method name depends on if a prefix or a suffix is provided
-  def prefix_or_suffix(enum_name)
-    enum_values = send(enum_name.to_s.pluralize).keys
-    enum_scopes = (methods - ActiveRecord::Base.methods).map(&:to_s).select do |method_name|
-      enum_values.select { |enum_value| method_name.include? enum_value }.present?
-    end
-
-    # no _prefix, no _suffix
-    expected_scopes = enum_values + enum_values.map { |enum_value| "not_#{enum_value}" }
-    return [nil, nil] if (expected_scopes - enum_scopes).blank?
-
-    # _prefix: true
-    expected_scopes = enum_values.map { |enum_value| "#{enum_name}_#{enum_value}" } +
-                      enum_values.map { |enum_value| "not_#{enum_name}_#{enum_value}" }
-    return [enum_name, nil] if (expected_scopes - enum_scopes).blank?
-
-    # _suffix: true
-    expected_scopes = enum_values.map { |enum_value| "#{enum_value}_#{enum_name}" } +
-                      enum_values.map { |enum_value| "not_#{enum_value}_#{enum_name}" }
-    return [nil, enum_name] if (expected_scopes - enum_scopes).blank?
-
-    # _prefix: string or _suffix: 'string'
-    named_prefix_or_suffix = named_prefix_or_suffix(enum_scopes, enum_values)
-    return named_prefix_or_suffix if named_prefix_or_suffix
-
-    raise ArgumentError, "Cannot create temporal scopes for #{enum_name}"
-  end
-
-  def named_prefix_or_suffix(enum_scopes, enum_values)
-    results = enum_scopes
-    results.each { |res| res.gsub!('not_', '') }
-    enum_values.each do |enum_value|
-      results.each { |res| res.gsub!(enum_value, '') }
-    end
-    results.uniq! # can be 'prefix_' or '_suffix' or 'prefix__suffix'
-    results = results.sole.split('_')
-    case results.length
-    when 1
-      [results.first, nil]
-    when 2
-      [nil, results.last]
-    when 3
-      [results.first, results.last]
-    end
-  rescue Enumerable::SoleItemExpectedError
-    nil
   end
 
   def check_enum_values!(enum_name)
---
+++ test/temporal_enum_with_positional_argument_test.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+ActiveRecord::Schema.define do
+  create_table :dummy_class_with_positional_arguments, force: true do |t|
+    t.integer :status
+  end
+end
+
+class DummyClassWithPositionalArgument < ActiveRecord::Base
+  enum :status, { created: 0, processing: 1, finished: 2 }, _temporal: true
+end
+
+class TemporalEnumWithPositionalArgument < Minitest::Test
+  def test_scopes_with_positional_argument
+    DummyClassWithPositionalArgument.create(status: 'created')
+    DummyClassWithPositionalArgument.create(status: 'processing')
+    DummyClassWithPositionalArgument.create(status: 'finished')
+
+    assert_equal 1, DummyClassWithPositionalArgument.created.count
+    assert_equal 1, DummyClassWithPositionalArgument.processing.count
+    assert_equal 1, DummyClassWithPositionalArgument.finished.count
+
+    assert_equal 0, DummyClassWithPositionalArgument.before_created.count
+    assert_equal 1, DummyClassWithPositionalArgument.before_or_created.count
+    assert_equal 2, DummyClassWithPositionalArgument.after_created.count
+    assert_equal 3, DummyClassWithPositionalArgument.after_or_created.count
+
+    assert_equal 1, DummyClassWithPositionalArgument.before_processing.count
+    assert_equal 2, DummyClassWithPositionalArgument.before_or_processing.count
+    assert_equal 1, DummyClassWithPositionalArgument.after_processing.count
+    assert_equal 2, DummyClassWithPositionalArgument.after_or_processing.count
+
+    assert_equal 2, DummyClassWithPositionalArgument.before_finished.count
+    assert_equal 3, DummyClassWithPositionalArgument.before_or_finished.count
+    assert_equal 0, DummyClassWithPositionalArgument.after_finished.count
+    assert_equal 1, DummyClassWithPositionalArgument.after_or_finished.count
+  end
+end
